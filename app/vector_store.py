83029371accb029dc03bbe6a27eb281604a06e54
Leelakrishna K <kleelakrishna753@gmail.com>
2025-05-11 18:18:55 +0530

    code

--- app/vector_store.py
+++ app/vector_store.py
@@ -1,13 +1,22 @@
 # app/vector_store.py
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from langchain.vectorstores import FAISS
+
+class MyEmbeddings:
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name)
+
+    def embed_documents(self, texts):
+        return self.model.encode(texts, show_progress_bar=True).tolist()
+
+    def embed_query(self, text):
+        return self.model.encode([text])[0].tolist()
 
 def create_vector_store(documents, persist_directory="db"):
-    model_name = "all-MiniLM-L6-v2"
-    embeddings = HuggingFaceEmbeddings(model_name=model_name)
-    
+    embeddings = MyEmbeddings()
     vectordb = FAISS.from_documents(documents, embeddings)
     return vectordb
+
 
 
 

d0799737dfe231b620ae3d385a8c2231ef8bc208
Leelakrishna K <kleelakrishna753@gmail.com>
2025-05-11 18:09:16 +0530

    code

--- app/vector_store.py
+++ app/vector_store.py
@@ -1,15 +1,13 @@
 # app/vector_store.py
-from sentence_transformers import SentenceTransformer
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-# Set up the sentence transformer model
 def create_vector_store(documents, persist_directory="db"):
-    model_name = "all-MiniLM-L6-v2"  # lightweight, fast, and free
+    model_name = "all-MiniLM-L6-v2"
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
     
-    vectordb = Chroma.from_documents(documents, embeddings, persist_directory=persist_directory)
-    vectordb.persist()
+    vectordb = FAISS.from_documents(documents, embeddings)
     return vectordb
+
 
 
